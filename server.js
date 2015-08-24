0f2ef1fd3c1f938dfc4480430f944fb66774344f
zchen9 <zhao.zchen9@gmail.com>
2015-08-24 14:22:19 +0800

    nodebeginner

--- server.js
+++ server.js
@@ -4,15 +4,37 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Requset for " + pathname +" received.");
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname);
-    response.write(content);
-    response.end();
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
   }
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
+
+// function start(route, handle) {
+//   function onRequest(request, response) {
+//     var postData = "";
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Requset for " + pathname +" received.");
+//
+//     // 接收数据的编码格式为UTF-8
+//     request.setEncoding("utf8");
+//
+//     // 注册"data"事件的监听器，用于手机每次接收到的新数据块
+//     request.addListener("data", function(postDataChunk) {
+//       postData += postDataChunk;
+//       console.log("Received POST data chunk '"
+//                   + postDataChunk
+//                   + "'.");
+//     });
+//
+//     request.addListener("end", function() {
+//       route(handle, pathname, response, postData);
+//     });
+//
+//   }
+//   http.createServer(onRequest).listen(8888);
+//   console.log("Server has started.");
+// }
 
 exports.start = start;
